1a5af3e8d050fa55a5cae2bca6f65852c3688a33
Clems11590 <c.balmisa@sunroad-equipment.com>
2026-02-06 09:09:47 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EmailManagementTool from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EmailManagementTool />
+  </React.StrictMode>,
+)
